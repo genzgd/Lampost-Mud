d8a7107d65828a960d9fab891c1f5d0253f0a2df
Geoffrey <genzgd@gmail.com>
2012-04-07 12:14:03 -0600

    Clean up argument handling, fix createimm script, add db password capability

--- main.py
+++ main.py
@@ -9,23 +9,32 @@ from lampost.mud import MudNature
 from lampost.context import Context
 
 
-if __name__ == "__main__":
-    port = 2500
-    db_num = 0
-    db_host = "localhost"
-    db_port = 6379
-    try:
-        for arg in sys.argv:
-            name, value = arg.split(":")
-            if name == "port":
-                port = int(value)
-            elif name == "db_host":
-                db_host = value
-            elif name == "db_port":
-                db_port = int(value)
-            elif name == "db_num":
-                db_num = int(db_num)
-    except Exception:
-        print "Invalid argument: " + name
-    else:                
-        Context(MudNature(), port, db_host, db_port, db_num).start()
+if __name__ != "__main__":
+    print "Invalid usage"
+    sys.exit(2)
+    
+port = 2500
+db_num = 0
+db_host = "localhost"
+db_port = 6379
+db_pw = None
+try:
+    for arg in sys.argv[1:]:
+        name, value = arg.split(":")
+        if name == "port":
+            port = int(value)
+        elif name == "db_host":
+            db_host = value
+        elif name == "db_port":
+            db_port = int(value)
+        elif name == "db_num":
+            db_num = int(db_num)
+        elif name == "db_pw":
+            db_pw = value
+        else:
+            print "Invalid argument: " + name
+            sys.exit(2)
+except ValueError:
+    print "Invalid argument format: " + arg
+    sys.exit(2)
+Context(MudNature(), port, db_host, db_port, db_num).start()
