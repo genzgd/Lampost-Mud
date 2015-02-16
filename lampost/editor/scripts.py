f426438cf37e08e702bc566817041334786df5e9
Geoffrey <genzgd@gmail.com>
2015-02-15 19:52:02 -0700

    Cleanup old modules.

--- lampost/editor/scripts.py
+++ lampost/editor/scripts.py
@@ -1,4 +1,4 @@
-from lampost.client.handlers import MethodHandler
+from lampost.server.handlers import MethodHandler
 from lampost.context.resource import m_requires
 from lampost.datastore.exceptions import DataError
 from lampost.gameops.script import compile_script
